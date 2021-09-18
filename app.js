ef387ed27ecd1373c883bac7d47a40d8e256b6ba
Dan Fletcher <dannyjfletcher@outlook.com>
2021-09-18 19:30:29 -0400

    Fix timestamp issue

--- app.js
+++ app.js
@@ -2,13 +2,32 @@ var express = require('express')
 var app = express()
 
 const getTimeStamp = (req, res) => {
-    const date = new Date(req.params.time)
-    res.json({
-        unix: Math.round(date.getTime() / 1000).toString(),
+    let date
+
+    if (!req.params.date) {
+        date = new Date()
+        const taco = { 
+            unix: date.getTime(), 
+            utc: date.toUTCString() 
+        }
+        console.log(taco)
+        return res.json(taco)
+    }
+
+    date = !isNaN(req.params.date)
+        ? new Date(Number(req.params.date))
+        : new Date(req.params.date)
+
+    const timeStamp = {
+        unix: date.getTime(),
         utc: date.toUTCString()
-    })
+    }
+    console.log(timeStamp)
+
+    res.json(timeStamp)
 };
-app.get('/api/:time', getTimeStamp)
+
+app.get('/api/:date?', getTimeStamp)
 
 module.exports = {
     getTimeStamp,
--- tests/getTimeStamp.spec.js
+++ tests/getTimeStamp.spec.js
@@ -1,11 +1,11 @@
 const { getTimeStamp } = require('../app.js')
 describe('timestamp api', () => {
 
-    it('returns a unix timestamp', () => {
+    it('returns correct unix timestamp with date string', () => {
         // given
         const req = {
             params: {
-                time: '2015-12-25'
+                date: '2015-12-25'
             }
         }
         let res
@@ -13,6 +13,34 @@ describe('timestamp api', () => {
         getTimeStamp(req, {json: (data) => {res = data}})
 
         // expect
-        expect(res).toStrictEqual({unix: '1451001600', utc: 'Fri, 25 Dec 2015 00:00:00 GMT'})
+        expect(res).toStrictEqual({"unix":1451001600000, "utc":"Fri, 25 Dec 2015 00:00:00 GMT"})
+    })
+
+    it('returns correct date string given unix timestamp', () => {
+        // given
+        const req = {
+            params: {
+                date: 1451001600000
+            }
+        }
+        let res
+        // when
+        getTimeStamp(req, {json: (data) => {res = data}})
+
+        // expect
+        expect(res).toStrictEqual({"unix":1451001600000, "utc":"Fri, 25 Dec 2015 00:00:00 GMT"})
+    })
+
+    it.skip('An empty date parameter should return the current time in a JSON object with a unix key', () => {
+        // given
+        const req = {
+            params: {}
+        }
+        let res
+        // when
+        getTimeStamp(req, {json: (data) => {res = data}})
+
+        // expect
+        expect(res).toStrictEqual({"unix":1451001600000, "utc":"Fri, 25 Dec 2015 00:00:00 GMT"})
     })
 })
